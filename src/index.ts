b661c4b25d48651f86041413acb6acfdc9f875f2
Matheus Jardim <matheus.jardim@conceptu.ind.br>
2023-10-18 23:29:40 -0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
-/**
- * The entrypoint for the action.
- */
-import { run } from './main'
+import core from '@actions/core'
+import validateResources from './run_checker'
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-run()
+async function runChecker(): Promise<void> {
+  try {
+    validateResources()
+  } catch (error) {
+    core.error(error as Error)
+    core.setFailed(error as Error)
+  }
+}
+
+runChecker()
